03d27c9f0089eab698a7c464df252a060f9db7f6
Md. Toufiqul Islam Tanmoy <67703225+toufiqulislamtanmoy@users.noreply.github.com>
2025-03-11 15:20:35 +0600

    repo setup

---
+++ app/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div className='text-green-500 text-center text-3xl'>
+      This is home
+    </div>
+  );
+};
+
+export default Home;
